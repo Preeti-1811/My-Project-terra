7c7486416b0bb7af0eb6d347c9b91b4784daa504
Preeti <preeti03545@gmail.com>
2026-08-24 09:56:04 +0530

    Add Terraform networking config (VPC, subnets, IGW, NAT)

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+#reusable values 
+
+variable "project_name" {
+  default = "my-project-terra"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b"]
+}
